d80447b1806caddd78929f1dcf75700eecb97689
Samet Demiralay <demiralayahmetsamet@gmail.com>
2023-11-29 22:19:25 +0300

    12-Delete Post

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,31 +1,47 @@
-import { postData } from '@/data';
 import Post from '@/components/Post';
 import Link from 'next/link';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
+import { TPost } from '../types';
+
+const getPosts = async (email: string) => {
+  try {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/authors/${email}`);
+    const { posts } = await res.json();
+    return posts;
+  } catch (error) {
+    return null;
+  }
+};
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
+  let posts = [];
 
   if (!session) {
     redirect('/sign-in');
+  }
+
+  if (email) {
+    posts = await getPosts(email);
   }
 
   return (
     <div>
       <h1>My Posts</h1>
 
-      {postData && postData.length > 0 ? (
-        postData.map((post) => (
+      {posts && posts.length > 0 ? (
+        posts.map((post: TPost) => (
           <Post
             key={post.id}
             id={post.id}
-            author={post.author}
-            authorEmail={'test@gmail.com'}
-            date={post.datepublished}
-            thumbnail={post.thumbnail}
-            category={post.category}
+            author={''}
+            authorEmail={post.authorEmail}
+            date={post.createdAt}
+            thumbnail={post.imageUrl}
+            category={post.catName}
             title={post.title}
             content={post.content}
             links={post.links || []}
@@ -33,8 +49,8 @@ export default async function Dashboard() {
         ))
       ) : (
         <div className='py-6'>
-          No posts created yet.
-          <Link className='underline ms-2' href={'/create-post'}>
+          No posts created yet.{' '}
+          <Link className='underline' href={'/create-post'}>
             Create New
           </Link>
         </div>
